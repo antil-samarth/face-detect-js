3dbe4d38875a3ec94a06be21d82d5a2e2e1b25fd
antil-samarth <64965361+antil-samarth@users.noreply.github.com>
2023-07-24 13:08:56 +0530

    Update haar.js

--- js/haar.js
+++ js/haar.js
@@ -2,7 +2,7 @@ let isFaceDetection = true; // Flag to indicate whether face detection is enable
 
 function openCvReady() {
     cv['onRuntimeInitialized'] = () => {
-        let video = document.getElementById("cam_input"); 
+        let video = document.getElementById("cam_input"); // video is the id of video tag
         navigator.mediaDevices.getUserMedia({ video: true, audio: false })
         .then(function(stream) {
             video.srcObject = stream;
@@ -16,22 +16,18 @@ function openCvReady() {
         let gray = new cv.Mat();
         let cap = new cv.VideoCapture(cam_input);
         let faces = new cv.RectVector();
-        let faceClassifier = new cv.CascadeClassifier();
-        let utils = new Utils('errorMessage');
-        let faceCascadeFile = 'haarcascade_frontalface_default.xml'; 
-        utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
-            faceClassifier.load(faceCascadeFile); 
-
         let eyes = new cv.RectVector();
+        let faceClassifier = new cv.CascadeClassifier();
         let eyeClassifier = new cv.CascadeClassifier();
-        let eyeCascadeFile = 'haarcascade_eye.xml';
+        let utils = new Utils('errorMessage');
+        let faceCascadeFile = 'haarcascade_frontalface_default.xml'; // path to face cascade xml
+        let eyeCascadeFile = 'haarcascade_eye.xml'; // path to eye cascade xml
+        utils.createFileFromUrl(faceCascadeFile, faceCascadeFile, () => {
+            faceClassifier.load(faceCascadeFile); // in the callback, load the face cascade from file
+        });
         utils.createFileFromUrl(eyeCascadeFile, eyeCascadeFile, () => {
             eyeClassifier.load(eyeCascadeFile); // in the callback, load the eye cascade from file
         });
-        });
-
-        
-        
         const FPS = 24;
 
         function processVideo() {
@@ -39,38 +35,38 @@ function openCvReady() {
             cap.read(src);
             src.copyTo(dst);
             cv.cvtColor(dst, gray, cv.COLOR_RGBA2GRAY, 0);
-
-            
-            try {
-                faceClassifier.detectMultiScale(gray, faces, 1.1, 3, 0);
-                eyeClassifier.detectMultiScale(gray, eyes, 1.1, 3, 0);
-
-                /* console.log(faces.size()); */
-            } catch (err) {
-                console.log(err);
-            }
-            for (let i = 0; i < faces.size(); ++i) {
-                let face = faces.get(i);
-                let point1 = new cv.Point(face.x, face.y);
-                let point2 = new cv.Point(face.x + face.width, face.y + face.height);
-                cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
-            }
-            for (let i = 0; i < eyes.size(); ++i) {
+                try {
+                    faceClassifier.detectMultiScale(gray, faces, 1.1, 3, 0);
+                    eyeClassifier.detectMultiScale(gray, eyes, 1.1, 3, 0);
+                    console.log(eyes.size());
+                } catch (err) {
+                    console.log(err);
+                }
+                for (let i = 0; i < faces.size(); ++i) {
+                    let face = faces.get(i);
+                    let point1 = new cv.Point(face.x, face.y);
+                    let point2 = new cv.Point(face.x + face.width, face.y + face.height);
+                    cv.rectangle(dst, point1, point2, [255, 0, 0, 255]);
+                }
+                for (let i = 0; i < eyes.size(); ++i) {
                     let eye = eyes.get(i);
                     let point1 = new cv.Point(eye.x, eye.y);
                     let point2 = new cv.Point(eye.x + eye.width, eye.y + eye.height);
                     cv.rectangle(dst, point1, point2, [0, 255, 0, 255]);
-            }
+                }
+
             if (faces.size() === 0) {
                 cv.putText(dst, "Please look at the camera", { x: 50, y: 50 }, cv.FONT_HERSHEY_SIMPLEX, 1.0, [0, 0, 255, 255], 2);
             }
-            
 
             cv.imshow("canvas_output", dst);
 
+            // schedule next one.
             let delay = 1000 / FPS - (Date.now() - begin);
             setTimeout(processVideo, delay);
         }
+
+        // schedule first one.
         setTimeout(processVideo, 0);
     };
 }
